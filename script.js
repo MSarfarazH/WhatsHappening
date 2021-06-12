fc02526cde122d7fa627fe7bb9a2897b9e2306ec
AlbertoCanales1 <albertocanales1@outlook.com>
2021-06-12 14:06:31 -0500

    Yelp API for singular events added

--- script.js
+++ script.js
@@ -21,19 +21,27 @@ var getEvents = function(){
     .then(response => response.json())
     .then(data => console.log(data));
  }
-// /// Google API///
-// var getEvent = function(){
-//     var apiUrl = 'https://api.yelp.com/v3/events/san-francisco-yelp-10-year-anniversary-celebration'
 
-//     fetch(event_site_url).then(function (response){
-//         if (response.ok) {
-//          response.json().then(function (data){
-//              displayEvent(data);
-//          })
-//         }
-//     })
 
-// }
+ 
+/// Singular Event Data ///
+var getEvent = function(){
+    
+     
+    fetch( corsApiUrl +'https://api.yelp.com/v3/events/san-francisco-yelp-10-year-anniversary-celebration', {
+    method: 'GET',
+    headers: new Headers({
+        'Authorization': 'Bearer 4FnW2xv5cVqRkDlYYF14_XYL6MmRVN661ctTKgsXOnD5ihmrJ2hqvCoMu0ItSgRlvByZexlvH8QeKxo6XZ8BUvgQolMKmUmsv1ijEsLLdK34XvK0rYqscbmy9wDAYHYx',
+        // "Content-type": "application/json"
+    })
+    
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+}
+
+getEvent()
 
 getEvents()
 
