5e05b652cc409f33c4ed3af96572f4d8dea0e713
AlbertoCanales1 <albertocanales1@outlook.com>
2021-06-12 11:04:52 -0500

    Working yelp source information

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-fetch('https://api.yelp.com/v3/events', {
-method: 'post',
-headers: new Headers({
-    'Authorization': 'Bearer 4FnW2xv5cVqRkDlYYF14_XYL6MmRVN661ctTKgsXOnD5ihmrJ2hqvCoMu0ItSgRlvByZexlvH8QeKxo6XZ8BUvgQolMKmUmsv1ijEsLLdK34XvK0rYqscbmy9wDAYHYx',
-}),
 
-})
-.then(response => response.json())
-.then(data => console.log(data));
-///Authorization
+const corsApiUrl = 'https://cors-anywhere.herokuapp.com/'
+
 
 
 var EventNameEl = document.querySelector('#event-name');
 
 // This layer will communicate with google.//
 var getEvents = function(){
-    var apiUrl = 'https://api.yelp.com/v3/events';
+    
+    fetch( corsApiUrl +'https://api.yelp.com/v3/events', {
+    method: 'GET',
+    headers: new Headers({
+        'Authorization': 'Bearer 4FnW2xv5cVqRkDlYYF14_XYL6MmRVN661ctTKgsXOnD5ihmrJ2hqvCoMu0ItSgRlvByZexlvH8QeKxo6XZ8BUvgQolMKmUmsv1ijEsLLdK34XvK0rYqscbmy9wDAYHYx',
+        // "Content-type": "application/json"
+    })
+    
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
  }
-
+/// Google API///
 var getEvent = function(){
     var apiUrl = 'https://api.yelp.com/v3/events/san-francisco-yelp-10-year-anniversary-celebration'
 
@@ -28,3 +31,8 @@ var getEvent = function(){
         }
     })
 
+}
+
+var displayEvent = function(){
+
+}
