db675d4e01e10a183349731316d6341a4070a807
Melissa Brewer <melramirez727@gmail.com>
2021-06-14 23:23:25 -0500

    call events

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-fetch('https://api.yelp.com/v3/events', {
-method: 'post',
-headers: new Headers({
-    'Authorization': 'Bearer 4FnW2xv5cVqRkDlYYF14_XYL6MmRVN661ctTKgsXOnD5ihmrJ2hqvCoMu0ItSgRlvByZexlvH8QeKxo6XZ8BUvgQolMKmUmsv1ijEsLLdK34XvK0rYqscbmy9wDAYHYx',
-}),
-
-})
-.then(response => response.json())
-.then(data => console.log(data));
-///Authorization
-
-
+console.log("Hello World")
+const corsApiUrl = 'https://cors-anywhere.herokuapp.com/'
+var eventsContainerEl = document.querySelector("#events-container");
 var EventNameEl = document.querySelector('#event-name');
+var submitBtnEl = document.querySelector('#submitBtn');
+var zipInputEl = document.querySelector('#zipInput');
+var eventOneEl = document.querySelector('#eventOne');
+var eventTwoEl = document.querySelector('#eventTwo');
+var eventThreeEl = document.querySelector('#eventThree');
 
-// This layer will communicate with google.//
+
+// This layer will call the events
 var getEvents = function(){
-    var apiUrl = 'https://api.yelp.com/v3/events';
- }
-
-var getEvent = function(){
-    var apiUrl = 'https://api.yelp.com/v3/events/san-francisco-yelp-10-year-anniversary-celebration'
-
-    fetch(event_site_url).then(function (response){
-        if (response.ok) {
-         response.json().then(function (data){
-             displayEvent(data);
-         })
-        }
+    var events ="san-francisco-yelp-10-year-anniversary-celebration ";
+    var zipCode =zipInputEl.value;
+    fetch( corsApiUrl +'https://api.yelp.com/v3/events?location='+zipCode, {
+    method: 'GET',
+    headers: new Headers({
+        'Authorization': 'Bearer 4FnW2xv5cVqRkDlYYF14_XYL6MmRVN661ctTKgsXOnD5ihmrJ2hqvCoMu0ItSgRlvByZexlvH8QeKxo6XZ8BUvgQolMKmUmsv1ijEsLLdK34XvK0rYqscbmy9wDAYHYx',
+        // "Content-type": "application/json"
     })
+    })
+    .then(response => response.json())
+    .then(function(data){
 
+        //log full response
+        console.log(data);
+        //formatted Event
+        var eventOneName = data.events[0].name 
+        console.log(eventOneName)   
+        eventOneEl.textContent=eventOneName
+        
+        var eventTwoName = data.events[1].name 
+        console.log(eventTwoName)   
+        eventTwoEl.textContent=eventTwoName
+        
+        var eventThreeName = data.events[2].name 
+        console.log(eventThreeName)   
+        eventThreeEl.textContent=eventThreeName   
+
+            } );
+    
+ }
+ submitBtnEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    console.log("hello")
+    getEvents()
+  });
+  
+
+ 
